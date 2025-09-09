176f51e2718015f6a60537960b45661e89672b17
SaMMy <sheltonavelinojunior@gmailcom>
2025-09-09 09:47:57 +0200

    Adicao de uma nova pagina de css para a pasta empresa

--- View/Empresa/Home.php
+++ View/Empresa/Home.php
@@ -7,6 +7,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- CSS -->
+    <link rel="stylesheet" href="../../Style/empresa.css">
     <title>Home</title>
 </head>
 <body>
--- View/Empresa/empresa.php
+++ View/Empresa/empresa.php
@@ -25,7 +25,7 @@ if (!$id_utilizador || !$id_localizacao) {
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://getbootstrap.com/docs/5.3/assets/css/docs.css" rel="stylesheet">
     <!-- CSS -->
-    <link rel="stylesheet" href="../../Style/home.css">
+    <link rel="stylesheet" href="../../Style/empresa.css">
 
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
     <link href='https://cdn.boxicons.com/fonts/basic/boxicons.min.css' rel='stylesheet'>
